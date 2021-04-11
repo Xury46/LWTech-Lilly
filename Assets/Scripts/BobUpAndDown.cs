345f9fa269d8fd9303d99a59210dca3c5f82bd23
Xury Greer <Xury.Greer@Gmail.com>
2021-04-10 19:07:41 -0700

    Make Lilly bob up and down

---
+++ Assets/Scripts/BobUpAndDown.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BobUpAndDown : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.up * (Mathf.Sin(Time.time) + 1.0f);
+    }
+}
